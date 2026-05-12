c33d1db8a024976a621d7e474f3fff730225bfe7
Naufal Arif <na713634@gmail.com>
2026-05-12 17:01:45 +0700

    fungtion main

--- paradigmaoop.cpp
+++ paradigmaoop.cpp
@@ -23,3 +23,11 @@ class Mahasiswa {
             cout << "Nilai: " << nilai << endl;
         }
 };
+
+int main()
+{
+    //deklarasi dan pemberian nilai untuk objek mhs
+    Mahasiswa mhs(12, "Asroni", 90.5);
+
+    return 0;
+}
