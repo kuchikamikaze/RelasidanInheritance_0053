4bd7f88f120276a2fc5000b3f0aadb0befd537b6
Naufal Arif <na713634@gmail.com>
2026-05-12 17:00:59 +0700

    membuat class mahasiswa beserta member properties dan constructor serta destructor

---
+++ paradigmaoop.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+//deklarasi class mahasiswa
+class Mahasiswa {
+    //membuat member class private
+    private:
+        const int id;
+        string nama;
+        float nilai;
+
+    //membuat member class public
+    public:
+        //membuat member initialization list dari class mahasiswa
+        Mahasiswa(int pId, string pNama, float pNilai) : nama(pNama), nilai(pNilai), id(pId) {
+            
+        };
+
+        //membuat destructor dari class mahasiswa
+        ~Mahasiswa() {
+            cout << "ID  : " << id << endl;
+            cout << "Nama : " << nama << endl;
+            cout << "Nilai: " << nilai << endl;
+        }
+};
