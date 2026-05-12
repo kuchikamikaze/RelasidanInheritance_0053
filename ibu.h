2abe199d48076c0042964db7b3796e3c396e54a7
Naufal Arif <na713634@gmail.com>
2026-05-12 15:44:30 +0700

    membuat kelas ibu dan menambahkan properties

--- ibu.h
+++ ibu.h
@@ -2,3 +2,11 @@
 #define IBU_H
 
 class anak;
+
+//membuat class ibu pada file header ibu.h
+class Ibu {
+public:
+    //deklarasi member variabel pada class ibu
+    string nama;
+    vector<anak*> daftar_anak;
+
