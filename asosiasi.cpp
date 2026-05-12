cf393a5d38835010c968d01d74d6420ad4781300
Naufal Arif <na713634@gmail.com>
2026-05-12 16:01:37 +0700

    deklarasi class pasien dan propertiesnya

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -5,3 +5,10 @@ using namespace std;
 //deklaras class dokter
 class dokter;
 
+//deklarasi dan pembuatan class pasien
+class pasien {
+public:
+    //deklarasi member variabel dari class pasien
+    string nama;
+    vector<dokter*> daftar_dokter;
+
