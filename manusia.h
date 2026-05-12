a9a511a2fbd562793c58fe01d30ec3811dc39de7
Naufal Arif <na713634@gmail.com>
2026-05-12 16:56:01 +0700

    libraru dan namespace

--- manusia.h
+++ manusia.h
@@ -1,6 +1,10 @@
 #ifndef MANUSIA_H
 #define MANUSIA_H
 
+#include <iostream>
+#include "jantung.h"
+using namespace std;
+
 //membuat class manusia di file header manusia.h
 class Manusia {
 public:
