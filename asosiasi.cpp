107b6f88f64ccc70cbbd727f952f6456da9946c8
Naufal Arif <na713634@gmail.com>
2026-05-12 16:45:35 +0700

    pemanggilan prosedur cetak pasien

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -89,3 +89,9 @@ int main()
     varDokter1->tambahPasien(varPasien1);
     varDokter1->tambahPasien(varPasien2);
     varDokter2->tambahPasien(varPasien1);
+
+    //pemanggilan prosedur cetakPasien() dari class dokter
+    varDokter1->cetakPasien();
+    varDokter2->cetakPasien();
+    varPasien1->cetakDokter();
+    varPasien2->cetakDokter();
